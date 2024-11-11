b5e189e97df78d3e21178528fe8af3b04fc820c0
Art <teamkait1@gmail.com>
2024-11-11 23:11:22 +0300

    19

---
+++ Упр19/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Упр19
+{
+    internal class Program
+    {
+        public static int Equation(int x)
+        {
+            return 2 * (x - 4) / (x + 5);
+        }
+        public static double Equation(double x)
+        {
+            return 2 * (x - 4) / (x + 5);
+        }
+        static void Main(string[] args)
+        {
+            Utils.PrintAuthors();
+            string input = Utils.Input<string>("число");
+            if(double.TryParse(input, out double result))
+            {
+                if(result == (int)result)
+                {
+                    Equation((int)result).Output("результат");
+                }
+                else
+                {
+                    Equation(result).Output("результат");
+                }
+            }
+            else
+            {
+                Utils.PrintError("Не введено число");
+            }
+        }
+    }
+}
